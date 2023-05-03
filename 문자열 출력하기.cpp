a4a305516097d39e7915713b17dfe52eb6383d56
songwooju <37572006+songwooju@users.noreply.github.com>
2023-05-03 19:21:33 +0900

    문자열 출력하기
    문자열 출력하기

---
+++ 문자열 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    getline(cin, str); // 문자열을 입력받음
+    cout << str << endl; // 입력받은 문자열 출력
+    return 0;
+}
